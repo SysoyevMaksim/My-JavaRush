0736c52e377536f7acfc92a4a5a96ed6829edb15
Maksim <sysoyev.maxim@gmail.com>
2020-06-11 23:32:35 +0300

    Java Core 2 уровень 9 лекция
    Не до конца

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        print(2);
+    }
+
+    //Напишите тут ваши методы
+    static void print(int i){}
+    static void print(Integer i){}
+}
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print(int i){}
+    static void print(int[] ints){}
+    static void print(Integer integer){}
+    static void print(String s){}
+    static void print(String[] strings){}
+}
