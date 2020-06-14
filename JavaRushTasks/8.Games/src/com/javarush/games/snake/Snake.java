d22712698a02598ab3f5a0f5de3166238a909d0d
Gr.Maksim <sysoyev.maxim@gmail.com>
2020-06-14 16:55:59 +0300

    Snake(15/20)

--- JavaRushTasks/8.Games/src/com/javarush/games/snake/Snake.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/snake/Snake.java
@@ -6,20 +6,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
+    public boolean isAlive = true;
     private List<GameObject> snakeParts = new ArrayList<GameObject>();
     private static final String HEAD_SIGN = "\uD83D\uDC7E";
-    private static final String BODY_SIGN  = "\u26AB";
-    public Snake(int x, int y){
+    private static final String BODY_SIGN = "\u26AB";
+    private Direction direction = Direction.LEFT;
+
+    public void setDirection(Direction direction) {
+        if ((direction == Direction.DOWN && this.direction != Direction.UP) ||
+                (direction == Direction.UP && this.direction != Direction.DOWN) ||
+                (direction == Direction.RIGHT && this.direction != Direction.LEFT) ||
+                (direction == Direction.LEFT && this.direction != Direction.RIGHT)) {
+            this.direction = direction;
+        }
+    }
+
+    public Snake(int x, int y) {
         snakeParts.add(new GameObject(x, y));
         snakeParts.add(new GameObject(x + 1, y));
         snakeParts.add(new GameObject(x + 2, y));
     }
-    public void draw(Game game){
+
+    public void draw(Game game) {
+        Color color;
+        if (isAlive) {
+            color = Color.BLACK;
+        } else {
+            color = Color.RED;
+        }
         GameObject gameObject = snakeParts.get(0);
-        game.setCellValue(gameObject.x, gameObject.y, HEAD_SIGN);
-        for (int i = 1; i < snakeParts.size(); i++){
+        game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, HEAD_SIGN, color, 75);
+        for (int i = 1; i < snakeParts.size(); i++) {
             gameObject = snakeParts.get(i);
-            game.setCellValue(gameObject.x, gameObject.y, BODY_SIGN);
+            game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, BODY_SIGN, color, 75);
         }
     }
+
+    public void move(Apple apple) {
+        GameObject Head = createNewHead();
+        if (Head.x < 0 || Head.x >= SnakeGame.WIDTH || Head.y < 0 || Head.y >= SnakeGame.HEIGHT) {
+            isAlive = false;
+            return;
+        }
+        if (checkCollision(Head)){
+            isAlive = false;
+            return;
+        }
+        snakeParts.add(0, Head);
+        if (Head.x == apple.x && Head.y == apple.y){
+            apple.isAlive = false;
+        }
+        else{
+            removeTail();
+        }
+    }
+
+    public GameObject createNewHead() {
+        GameObject oldHead = snakeParts.get(0);
+        int x = 0, y = 0;
+        if (direction == Direction.DOWN) {
+            x = oldHead.x;
+            y = oldHead.y + 1;
+        }
+        if (direction == Direction.LEFT) {
+            x = oldHead.x - 1;
+            y = oldHead.y;
+        }
+        if (direction == Direction.RIGHT) {
+            x = oldHead.x + 1;
+            y = oldHead.y;
+        }
+        if (direction == Direction.UP) {
+            x = oldHead.x;
+            y = oldHead.y - 1;
+        }
+        return new GameObject(x, y);
+    }
+
+    public void removeTail() {
+        snakeParts.remove(snakeParts.size() - 1);
+    }
+
+    public boolean checkCollision(GameObject gameObject) {
+        for (GameObject gameObject1 : snakeParts) {
+            if (gameObject.y == gameObject1.y && gameObject.x == gameObject1.x) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- JavaRushTasks/8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -1,12 +1,13 @@
 package com.javarush.games.snake;
 
-import com.javarush.engine.cell.Color;
-import com.javarush.engine.cell.Game;
+import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
     private Snake snake;
+    private int turnDelay;
+    private Apple apple;
 
     @Override
     public void initialize() {
@@ -16,15 +17,50 @@ public class SnakeGame extends Game {
 
     private void createGame(){
         snake = new Snake(WIDTH/2, HEIGHT/2);
+        createNewApple();
         drawScene();
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
     }
 
     private void drawScene(){
         for (int i = 0; i < WIDTH; i++){
             for (int j = 0; j < HEIGHT; j++){
-                setCellColor(i, j, Color.DARKSEAGREEN);
+                setCellValueEx(i, j, Color.DARKSEAGREEN, "");
             }
         }
         snake.draw(this);
+        apple.draw(this);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        snake.move(apple);
+        if (!apple.isAlive){
+            createNewApple();
+        }
+        drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        if (key == Key.DOWN){
+            snake.setDirection(Direction.DOWN);
+        }
+        if (key == Key.LEFT){
+            snake.setDirection(Direction.LEFT);
+        }
+        if (key == Key.RIGHT){
+            snake.setDirection(Direction.RIGHT);
+        }
+        if (key == Key.UP){
+            snake.setDirection(Direction.UP);
+        }
+    }
+
+    private void createNewApple(){
+        int x = getRandomNumber(WIDTH);
+        int y = getRandomNumber(HEIGHT);
+        apple = new Apple(x, y);
     }
 }
